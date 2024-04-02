1a86088d2b18e3842607f4494926c8ccbfa3ea4e
Jakobson <kubatron117@gmail.com>
2024-04-02 14:25:48 +0200

    install active storage

--- after-install.sh
+++ after-install.sh
@@ -1,3 +1,4 @@
 ./rails.sh rails generate rodauth:install --json --argon2
 ./rails.sh rails generate simple_form:install
 ./rails.sh rails g simple_form:tailwind:install
+./rails.sh rails active_storage:install
--- scaffold.sh
+++ scaffold.sh
@@ -1,18 +1,10 @@
-docker-compose exec web rails g scaffold Genre name:string:uniq
-docker-compose exec web rails g scaffold Movie name:string original_name:string genre:references rating_score:float poster_path:string description:text
-docker-compose exec web rails g scaffold Director first_name:string last_name:string
-docker-compose exec web rails g scaffold Writer first_name:string last_name:string
-docker-compose exec web rails g scaffold Actor first_name:string last_name:string
-docker-compose exec web rails g scaffold Producer first_name:string last_name:string
-docker-compose exec web rails g scaffold Editor first_name:string last_name:string
-docker-compose exec web rails g scaffold Rating rating:integer comment:text account:references movie:references
-docker-compose exec web rails g scaffold Country name:string
-docker-compose exec web rails g scaffold Vod name:string link:string movie:references
-docker-compose exec web rails g scaffold Premier name:string date:date movie:references
-docker-compose exec web rails g migration CreateJoinTableMovieGenre movie genre
-docker-compose exec web rails g migration CreateJoinTableMovieCountry movie country
-docker-compose exec web rails g migration CreateJoinTableMovieDirector movie director
-docker-compose exec web rails g migration CreateJoinTableMovieWriter movie writer
-docker-compose exec web rails g migration CreateJoinTableMovieActor movie actor
-docker-compose exec web rails g migration CreateJoinTableMovieProducer movie producer
-docker-compose exec web rails g migration CreateJoinTableMovieEditor movie editor
+docker-compose exec web rails generate scaffold Region name:string code:string
+docker-compose exec web rails generate scaffold District name:string code:string region:references
+docker-compose exec web rails generate scaffold FireDepartment name:string code:string district:references address:string
+docker-compose exec web rails generate scaffold Member first_name:string last_name:string birthdate:date address:string email:string phone:string member_code:string role:string
+docker-compose exec web rails generate scaffold FireDepartmentMembership start_date:date fire_department:references member:references role:integer status:integer
+docker-compose exec web rails generate scaffold Award name:string award_kind:integer dependent_on_award:integer minimum_service_years:integer minimum_age_for_award:integer
+
+
+
+
