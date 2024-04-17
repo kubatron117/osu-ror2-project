05f27bd0da81eada8b63a6771ac9f60239ec766f
Jakobson <kubatron117@gmail.com>
2024-04-17 13:28:38 +0200

    test for awards

--- spec/models/award_spec.rb
+++ spec/models/award_spec.rb
@@ -2,15 +2,14 @@
 require 'rails_helper'
 
 RSpec.describe Award, type: :model do
-  it { should have_and_belong_to_many(:accounts) }
   it { should belong_to(:dependent_on_award).class_name('Award').optional }
+  it { should have_one_attached(:image) }
+  it { should have_many(:account_awards) }
 
+  it { should define_enum_for(:award_kind).with_values(acknowledgement: 0, medal: 1, magnitude: 2) }
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_most(100) }
   it { should validate_presence_of(:award_kind) }
-  it { should validate_inclusion_of(:award_kind).in_array(Award.award_kinds.keys) }
-  it { should validate_presence_of(:minimum_service_years) }
-  it { should validate_numericality_of(:minimum_service_years).is_greater_than_or_equal_to(0) }
-  it { should validate_presence_of(:minimum_age_for_award) }
-  it { should validate_numericality_of(:minimum_age_for_award).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:minimum_service_years).is_greater_than_or_equal_to(0).only_integer }
+  it { should validate_numericality_of(:minimum_age_for_award).is_greater_than_or_equal_to(0).only_integer }
 end
